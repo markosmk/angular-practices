c3498d17bcc664ce3075c9d664bb4f68737274b8
Markos <markosmk@me.com>
2022-10-28 10:01:20 -0300

    chore: add forms reactive to new module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
@@ -7,6 +8,6 @@ import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
 })
 export class AuthModule {}
