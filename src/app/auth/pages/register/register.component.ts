7619b3cd74a234118e9c81e940ce887e5706c0c2
Markos <markosmk@me.com>
2022-10-28 10:01:41 -0300

    chore: register with use service validator

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,16 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { ValidatorService } from 'src/app/shared/validator/validator.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup = this.fb.group({
+    name: [, [Validators.required, Validators.pattern(this.validatorSrv.fullNamePattern)]],
+    email: [, [Validators.required, Validators.pattern(this.validatorSrv.emailPattern)]],
+    username: [, [Validators.required, this.validatorSrv.validateUsername]],
+  });
+  constructor(private fb: FormBuilder, private validatorSrv: ValidatorService) {}
 
   ngOnInit(): void {
+    this.form.reset({
+      name: 'Marcos Scooby',
+      email: 'firulais@mail.com',
+      username: 'markosmk',
+    });
   }
 
+  validateField(field: string) {
+    return this.form.get(field)?.invalid && this.form.get(field)?.touched;
+  }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    console.log(this.form.value);
+  }
 }
