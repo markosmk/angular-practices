196833a095ce8c4c9babc81ef789db4a48279dcc
Markos <markosmk@me.com>
2022-10-29 10:38:50 -0300

    chore: add interface country

---
+++ src/app/selector/interfaces/country.interface.ts
@@ -0,0 +1,106 @@
+// form Quicktype
+export interface CountrySmall {
+  name: Name;
+  cca3: string;
+  capital: string[];
+  altSpellings: string[];
+}
+
+export interface Name {
+  common: string;
+  official: string;
+  nativeName: { [key: string]: NativeName };
+}
+
+export interface NativeName {
+  official: string;
+  common: string;
+}
+
+export interface Country {
+  name: Name;
+  tld: string[];
+  cca2: string;
+  ccn3: string;
+  cca3: string;
+  cioc: string;
+  independent: boolean;
+  status: string;
+  unMember: boolean;
+  currencies: Currencies;
+  idd: Idd;
+  capital: string[];
+  altSpellings: string[];
+  region: string;
+  subregion: string;
+  languages: Languages;
+  translations: { [key: string]: NativeName };
+  latlng: number[];
+  landlocked: boolean;
+  borders: string[];
+  area: number;
+  demonyms: Demonyms;
+  flag: string;
+  maps: Maps;
+  population: number;
+  gini: Gini;
+  fifa: string;
+  car: Car;
+  timezones: string[];
+  continents: string[];
+  flags: CoatOfArms;
+  coatOfArms: CoatOfArms;
+  startOfWeek: string;
+  capitalInfo: CapitalInfo;
+}
+
+export interface CapitalInfo {
+  latlng: number[];
+}
+
+export interface Car {
+  signs: string[];
+  side: string;
+}
+
+export interface CoatOfArms {
+  png: string;
+  svg: string;
+}
+
+export interface Currencies {
+  COP: Cop;
+}
+
+export interface Cop {
+  name: string;
+  symbol: string;
+}
+
+export interface Demonyms {
+  eng: Eng;
+  fra: Eng;
+}
+
+export interface Eng {
+  f: string;
+  m: string;
+}
+
+export interface Gini {
+  '2019': number;
+}
+
+export interface Idd {
+  root: string;
+  suffixes: string[];
+}
+
+export interface Languages {
+  spa: string;
+}
+
+export interface Maps {
+  googleMaps: string;
+  openStreetMaps: string;
+}
