630a3692442f67a04e69b1cdf13c93a2b2dd19d5
Markos <markosmk@me.com>
2022-10-29 10:38:27 -0300

    feat: reformat nested with rxjs switchmap

--- src/app/selector/pages/countries/countries.component.ts
+++ src/app/selector/pages/countries/countries.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { switchMap, tap } from 'rxjs';
+
+import { CountrySmall } from '../../interfaces/country.interface';
 import { CountryService } from '../../services/country.service';
 
 @Component({
@@ -9,15 +12,41 @@ import { CountryService } from '../../services/country.service';
 })
 export class CountriesComponent implements OnInit {
   form: FormGroup = this.fb.group({
-    continent: ['', Validators.required],
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    frontier: ['', Validators.required],
   });
 
   regions: string[] = [];
+  countries: CountrySmall[] = [];
+  frontiers: [] = [];
 
   constructor(private fb: FormBuilder, private countryService: CountryService) {}
 
   ngOnInit(): void {
     this.regions = this.countryService.regions;
+
+    // when change region
+    // this.form.get('region')?.valueChanges.subscribe((region) => {
+    //   console.log(region);
+    //   this.countryService.getCountriesByRegion(region).subscribe((items: CountrySmall[]) => {
+    //     console.log(items);
+    //     this.countries = items;
+    //   });
+    // });
+
+    // with rxjs
+    this.form
+      .get('region')
+      ?.valueChanges.pipe(
+        tap((_region) => {
+          this.form.get('country').reset('');
+        }), // generate effect secondary
+        switchMap((region) => this.countryService.getCountriesByRegion(region))
+      )
+      .subscribe((countries) => {
+        this.countries = countries;
+      });
   }
 
   save() {
