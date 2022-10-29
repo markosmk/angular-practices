48516b1b602504e7807a1452305d83bacb062460
Markos <markosmk@me.com>
2022-10-29 09:19:58 -0300

    feat: add http client global module and srv email

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -9,7 +10,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, SharedModule, AppRoutingModule, RouterModule],
+  imports: [BrowserModule, FormsModule, SharedModule, AppRoutingModule, RouterModule, HttpClientModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
