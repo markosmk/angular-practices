a31c7f7dc67cc5bd976292e88f89cb0b21232140
Markos <markosmk@me.com>
2022-10-29 23:46:58 -0300

    feat: added form reactive

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { DirectivesRoutingModule } from './directives-routing.module';
 import { AddComponent } from './pages/add/add.component';
 
-
 @NgModule({
-  declarations: [
-    AddComponent
-  ],
-  imports: [
-    CommonModule,
-    DirectivesRoutingModule
-  ]
+  declarations: [AddComponent],
+  imports: [CommonModule, DirectivesRoutingModule, ReactiveFormsModule],
 })
-export class DirectivesModule { }
+export class DirectivesModule {}
--- src/app/directives/pages/add/add.component.ts
+++ src/app/directives/pages/add/add.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class AddComponent implements OnInit {
+  form: FormGroup = this.fb.group({
+    name: [, Validators.required],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  validateField(field: string) {
+    return this.form.get(field).invalid && (this.form.get(field).touched || this.form.get(field).dirty);
   }
 
+  save() {
+    console.log(this.form.value);
+  }
 }
