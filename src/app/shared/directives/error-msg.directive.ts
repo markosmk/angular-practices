47a6e7c5dcb3da147f8fc5ef191d302d040a2335
Markos <markosmk@me.com>
2022-10-30 00:09:00 -0300

    chore: add text and class with directive

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -5,6 +5,7 @@ import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 })
 export class ErrorMsgDirective implements OnInit {
   @Input() color: string = 'red';
+  @Input() message: string = 'This field is required';
   htmlElement: ElementRef<HTMLElement>;
 
   constructor(private elem: ElementRef<HTMLElement>) {
@@ -13,9 +14,15 @@ export class ErrorMsgDirective implements OnInit {
 
   ngOnInit(): void {
     this.setColor();
+    this.setMessage();
   }
 
   setColor() {
+    this.htmlElement.nativeElement.classList.add('error');
     this.htmlElement.nativeElement.style.color = this.color;
+  }
+
+  setMessage(): void {
+    this.htmlElement.nativeElement.innerText = this.message;
   }
 }
