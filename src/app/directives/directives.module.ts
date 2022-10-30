d96638c12188b1aaa934db1d4028a6d47a1e912e
Markos <markosmk@me.com>
2022-10-30 00:03:38 -0300

    chore: use new directive to change color error

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -4,9 +4,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { DirectivesRoutingModule } from './directives-routing.module';
 import { AddComponent } from './pages/add/add.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [AddComponent],
-  imports: [CommonModule, DirectivesRoutingModule, ReactiveFormsModule],
+  imports: [CommonModule, DirectivesRoutingModule, ReactiveFormsModule, SharedModule],
 })
 export class DirectivesModule {}
